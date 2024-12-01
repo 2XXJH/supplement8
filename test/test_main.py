86cfd9818be2f8b8645fbc1be238d9a049569b50
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-30 22:31:59 -0500

    test: implemented test for hello

--- test/test_main.py
+++ test/test_main.py
@@ -7,22 +7,19 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from HTTP_functions import get_request
 from HTTP_functions import get_beeceptor_info
 
-
-
-#
-def test_get_request():
+def test_should_get_request():
     url = "https://jsonplaceholder.typicode.com/posts/1"  # A sample URL that returns JSON
     status_code, response_text = get_request(url)
     assert status_code == 200
     assert isinstance(response_text, dict)  # Should return a dictionary for JSON response
 
-def test_get_request_error():
+def test_should_get_request_error():
     url = "https://jsonplaceholder.typicode.com/invalid_url"
     with pytest.raises(Exception):
         get_request(url)
 
 @patch('requests.get')
-def test_get_beeceptor_info(mock_get):
+def test_should_get_beeceptor_info(mock_get):
     mock_response = requests.Response()
     mock_response.status_code = 200
     mock_response._content = b'{"Postman-Token": "abcd1234", "ipAddress": "192.168.1.1"}'
@@ -32,3 +29,14 @@ def test_get_beeceptor_info(mock_get):
 
     assert postman_token == "abcd1234"
     assert ip_address == "192.168.1.1"
+
+@patch('requests.post')
+def test_should_post_beeceptor_hello(mock_post):
+    mock_response = requests.Response()
+    mock_response.status_code = 200
+    mock_response._content = b'{"hello": "world"}'
+    mock_post.return_value = mock_response
+
+    response_json = post_beeceptor_hello()
+
+    assert response_json == {"hello": "world"}
